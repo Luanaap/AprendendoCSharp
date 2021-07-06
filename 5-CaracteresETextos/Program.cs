36e336178756d200f75b1dcefc198c9477e13d2c
Luana Andrade Pereira <l.andrade2711@gmail.com>
2021-07-06 11:38:29 -0300

    Check-in inicial do módulo AprendendoCSharp

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos!");
+
+            //character
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2021;
+            string cursosProgramacao =
+@" - .NET
+- JAVA  
+- JAVASCRIPT ";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 13 - For Encadeado!");
+
+            //Escrevendo asteriscos com o BREAK
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+
+            // Uma forma diferente de fazer o desenho de asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+
+                    Console.Write("*");                   
+
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
